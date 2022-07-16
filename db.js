ea53f09d067ef7f99666b716b0ac1b46b5cb2972
sanketjuet <55625295+sanketjuet@users.noreply.github.com>
2022-07-16 15:03:19 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,45 +1,4 @@
-/*M
-const mongoose = require("mongoose");
 
-var MONGO_URI =
-  "mongodb+srv://Sanket****************%40cluster0.p5u5g3q.mongodb.net/mern-rooms";
-
-module.exports = connectdb = () => {
-  mongoose
-    .connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    })
-
-    .then(() => {
-      console.log(`connection successful and db connected`);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-};
-*/
-
-/*
-const { default: mongoose } = require("mongoose");
-
-var mongoURL =
-  "mongodb+srv://Sanket:VMwP122NizOfSBHM@cluster0.p5u5g3q.mongodb.net/test";
-
-mongoose.connect(mongoURL, { useUnifiedTopology: true, useNewUrlParser: true });
-
-var connection = mongoose.connection;
-
-connection.on("error", () => {
-  console.log("Mongo DB connection failed");
-});
-
-connection.on("Connected", () => {
-  console.log("Mongo DB Connection Successful");
-});
-
-module.exports = mongoose;
-*/
 const mongoose = require("mongoose");
 
 var mongoDBURL =
